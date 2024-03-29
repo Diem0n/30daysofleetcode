51a2e886da955f06ef26fd9f702651c9c53b8f93
Talha <90450179+Diem0n@users.noreply.github.com>
2024-03-29 21:17:29 +0500

    Time: 43 ms (23.37%), Space: 16.6 MB (44.62%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,16 +1,24 @@
 class Solution:
+    @staticmethod
+    def dac(strs, left, right):
+        if left == right:
+            return strs[left]
+        else:
+            mid = (left + right) // 2
+            prefix_left = Solution.dac(strs, left, mid)
+            prefix_right = Solution.dac(strs, mid + 1, right)
+            return Solution.common_prefix(prefix_left, prefix_right)
+    @staticmethod
+    def common_prefix(str1, str2):
+        min_len = min(len(str1), len(str2))
+        for i in range(min_len):
+            if str1[i] != str2[i]:
+                return str1[:i]
+        return str1[:min_len]
+
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
+        return Solution.dac(strs, 0, len(strs) - 1)
 
-        p = strs[0]  
-
-        for s in strs[1:]:
-            while p:
-                if s.startswith(p):
-                    break
-                p = p[:-1]
-            else:
-                return ""
-
-        return p
+   
